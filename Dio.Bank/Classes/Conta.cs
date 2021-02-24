34698bd27895a61704193f6c7c4d00aa6fcd16dd
THAIS CARDOSO DE FARIAS <thais.farias@al.infnet.edu.br>
2021-02-24 18:51:21 -0300

    Adicionar arquivos de projeto.

---
+++ Dio.Bank/Classes/Conta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dio.Bank
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if(Saldo - valorSaque < Credito * -1)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + TipoConta + " | ";
+            retorno += "Nome " + Nome + " | ";
+            retorno += "Saldo " + Saldo + " | ";
+            retorno += "Crédito " + this.Credito;
+            return retorno;
+        }
+    }
+}
---
+++ Dio.Bank/Enum/TipoConta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dio.Bank
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+
+        PessoaJuridica = 2
+    }
+}
